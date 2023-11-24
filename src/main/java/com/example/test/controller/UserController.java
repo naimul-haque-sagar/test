9ed5c283dc3a94dd91c63d92aa156546c60cd972
naimul-haque-sagar <naimulhaquesagar24@gmail.com>
2023-11-24 21:45:04 +0600

    jpa config added

--- src/main/java/com/example/test/controller/UserController.java
+++ src/main/java/com/example/test/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.test.entity.User;
 import com.example.test.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import javax.validation.Valid;
 public class UserController {
     private final UserService userService;
 
+    @PostMapping("/add")
     public ResponseEntity<UserDto> addUser(@Valid @RequestBody UserDto userDto) {
         User user = userService.addNewUser(userDto);
 
